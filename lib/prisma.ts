40df4786456728d219f94d0795a67a4dee7cff3a
pouyatayebi <tayebi.pouya@gmail.com>
2025-06-07 17:32:25 +0330

    neondb

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,12 +1,47 @@
-import { PrismaClient } from './generated/prisma'
-import { withAccelerate } from '@prisma/extension-accelerate'
+// import { PrismaClient } from './generated/prisma'
+// import { withAccelerate } from '@prisma/extension-accelerate'
 
-const globalForPrisma = global as unknown as { 
-    prisma: PrismaClient
+// const globalForPrisma = global as unknown as { 
+//     prisma: PrismaClient
+// }
+
+// const prisma = globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate())
+
+// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+// export default prisma
+
+
+
+// lib/prisma.ts
+
+import { PrismaNeon } from '@prisma/adapter-neon';
+import { neonConfig } from '@neondatabase/serverless';
+import WebSocket from 'ws';
+import { PrismaClient } from './generated/prisma';
+
+// برای اینکه PrismaNeon بتواند از WebSocket استفاده کند
+neonConfig.webSocketConstructor = WebSocket;
+
+declare global {
+  // جلوگیری از ایجاد چند نمونه در محیط توسعه
+  var prisma: PrismaClient | undefined;
 }
 
-const prisma = globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate())
+const adapter = new PrismaNeon({
+  connectionString: process.env.DATABASE_URL!,
+});
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+export const prisma =
+  global.prisma ??
+  new PrismaClient({
+    adapter,
+    // log: ['query', 'info', 'warn', 'error'], // (اختیاری)
+  });
 
-export default prisma
+if (process.env.NODE_ENV !== 'production') {
+  global.prisma = prisma;
+}
+
+
+export default prisma;
